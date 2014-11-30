bb9809ceb557e5b767d94d1fbf57bbb7293a6a39
tanguy morvant <t.morvant@student.isartdigital.com>
2014-11-30 18:31:53 +0100

    Prototype map added

--- HeroLineWar/Assets/Scripts/Camera/CameraMovement.cs
+++ HeroLineWar/Assets/Scripts/Camera/CameraMovement.cs
@@ -5,9 +5,13 @@ public class CameraMovement : MonoBehaviour {
 
 	private Transform trans;
 	[SerializeField]
-	private float speed = 1;
+	private float speed = 1.0f;
 	[SerializeField]
 	private GameObject Player;
+    [SerializeField]
+    private float MaxZoom = 15.0f;
+    [SerializeField]
+    private float MinZoom = 90.0f;
 
 	void Awake()
 	{
@@ -20,36 +24,32 @@ public class CameraMovement : MonoBehaviour {
 	
 	void Update () 
 	{
-		KeyBoardDeplacement();
-		MouseDeplacement();
+		CameraMove();
+        Zoom();
 
 		if (Input.GetKey(KeyCode.Space))
 			CenterPlayer();
 	}
 
-	void MouseDeplacement()
+	void CameraMove()
 	{
-		if (Input.mousePosition.x >= 0 && Input.mousePosition.x <= 10)
+        if ((Input.mousePosition.x >= 0 && Input.mousePosition.x <= 10) || Input.GetKey(KeyCode.LeftArrow))
 			trans.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
-		else if (Input.mousePosition.x >= Screen.width - 10 && Input.mousePosition.x <= Screen.width)
+        else if ((Input.mousePosition.x >= Screen.width - 10 && Input.mousePosition.x <= Screen.width) || Input.GetKey(KeyCode.RightArrow))
 			trans.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
-		if (Input.mousePosition.y >= 0 && Input.mousePosition.y <= 10)
+        if ((Input.mousePosition.y >= 0 && Input.mousePosition.y <= 10) || Input.GetKey(KeyCode.DownArrow))
 			trans.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
-		else if (Input.mousePosition.y >= Screen.height - 10 && Input.mousePosition.y <= Screen.height)
+        else if ((Input.mousePosition.y >= Screen.height - 10 && Input.mousePosition.y <= Screen.height) || Input.GetKey(KeyCode.UpArrow))
 			trans.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
 	}
 
-	void KeyBoardDeplacement()
-	{
-		if (Input.GetKey(KeyCode.LeftArrow))
-			trans.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
-		if (Input.GetKey(KeyCode.UpArrow))
-			trans.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
-		if (Input.GetKey(KeyCode.RightArrow))
-			trans.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
-		if (Input.GetKey(KeyCode.DownArrow))
-			trans.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
-	}
+    void Zoom()
+    {
+        if (trans.position.y >= MaxZoom && Input.GetAxis("Mouse ScrollWheel") > 0.0f)
+            trans.Translate(Vector3.down, Space.World);
+        if (trans.position.y <= MinZoom && Input.GetAxis("Mouse ScrollWheel") < 0.0f)
+            trans.Translate(Vector3.up, Space.World);
+    }
 
 	void CenterPlayer()
 	{
